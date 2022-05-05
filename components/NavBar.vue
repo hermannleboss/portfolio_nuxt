64c8f6f7d631ae6da8bff41362e7f125f501bac0
EHO K. Hermann <hermannleboss@gmail.com>
2022-05-05 20:20:29 +0000

    Finish nav

---
+++ components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <b-nav class='c-nav' style=''>
+    <b-navbar-brand href="#">
+      <svg width="61" height="32" viewBox="0 0 61 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M31.3469 26.1224L15.6735 0L0 26.1224H31.3469ZM60.0816 5.87755L44.4082 32L28.7347 5.87755H60.0816Z" fill="#33323D"/>
+      </svg>
+    </b-navbar-brand>
+    <b-nav>
+      <b-nav-item active>HOME</b-nav-item>
+      <b-nav-item>PORTFOLIO</b-nav-item>
+      <b-nav-item>CONTACT ME</b-nav-item>
+    </b-nav>
+  </b-nav>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped lang='scss'>
+$color-primary: #5FB4A2;
+$color-secondary: #203A4C;
+$color-dark:  #33323D;
+$color-light:  #FAFAFA;
+$color-light-gray:  #EAEAEB;
+$color-red:  #F43030;
+.c-nav{
+  justify-content: space-between;
+    padding: 64px 165px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,70 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <NavBar/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
 export default Vue.extend({
   name: 'IndexPage',
 })
 </script>
+
+<style lang='scss'>
+$color-primary: #5FB4A2;
+$color-secondary: #203A4C;
+$color-dark:  #33323D;
+$color-light:  #FAFAFA;
+$color-light-gray:  #EAEAEB;
+$color-red:  #F43030;
+body{
+  font-family: 'Public Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 30px;
+  /* or 188% */
+  color:$color-dark;
+}
+h1{
+  font-family: 'Ibarra Real Nova';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 50px;
+  line-height: 50px;
+  /* or 100% */
+  letter-spacing: -0.446429px;
+  color: $color-dark;
+}
+
+h2{
+  font-family: 'Ibarra Real Nova';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 42px;
+  /* or 105% */
+
+  letter-spacing: -0.357143px;
+  color: $color-dark;
+}
+
+h3{
+  font-family: 'Ibarra Real Nova';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 32px;
+  line-height: 36px;
+  /* or 112% */
+  letter-spacing: -0.285714px;
+}
+
+a{
+  color: $color-dark;
+}
+.active{
+
+  color: $color-primary;
+}
+</style>
