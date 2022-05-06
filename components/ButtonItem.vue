baa54d147075e9e2d17e60b2f06dadf06a41440e
EHO K. Hermann <hermannleboss@gmail.com>
2022-05-06 20:23:31 +0000

    refactore code

--- components/ButtonItem.vue
+++ components/ButtonItem.vue
@@ -1,12 +1,17 @@
 <template>
-  <b-button squared variant="primary" class='d-flex  align-items-center'>
+  <b-button squared variant='primary' class='d-flex align-items-center'>
     <div class='btn-icon d-flex align-items-center justify-content-center'>
-      <svg width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M1 9L9 13L17 9" stroke="#FFF"/>
-        <path opacity="0.5" d="M1 5L9 9L17 5" stroke="#FFF"/>
-        <path opacity="0.25" d="M1 1L9 5L17 1" stroke="#FFF"/>
+      <svg
+        width='18'
+        height='14'
+        viewBox='0 0 18 14'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path d='M1 9L9 13L17 9' stroke='#FFF' />
+        <path opacity='0.5' d='M1 5L9 9L17 5' stroke='#FFF' />
+        <path opacity='0.25' d='M1 1L9 5L17 1' stroke='#FFF' />
       </svg>
-
     </div>
     <div class='btn-text'>
       <slot>About Me</slot>
@@ -16,31 +21,34 @@
 
 <script>
 export default {
-  name: 'ButtonItem'
+  name:         'ButtonItem',
+  inheritAttrs: false
 }
 </script>
 
 <style scoped lang='scss'>
+$color-primary: #5fb4a2;
+$color-secondary: #203a4c;
+$color-dark: #33323d;
+$color-light: #fafafa;
+$color-light-gray: #eaeaeb;
+$color-red: #f43030;
 
-$color-primary: #5FB4A2;
-$color-secondary: #203A4C;
-$color-dark:  #33323D;
-$color-light:  #FAFAFA;
-$color-light-gray:  #EAEAEB;
-$color-red:  #F43030;
-
-.btn{
+.btn {
   height: 48px;
   padding: 0;
-  &-primary{
-     background-color: $color-secondary;
-   }
-  &-icon{
+
+  &-primary {
+    background-color: $color-secondary;
+  }
+
+  &-icon {
     width: 48px;
     height: 48px;
-    color: $color-primary ;
+    color: $color-primary;
   }
-  &-text{
+
+  &-text {
     padding: 17px;
   }
 }
