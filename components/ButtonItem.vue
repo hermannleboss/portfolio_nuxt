cb70ed9efb80a7ac694bc4bb22ba22c24322b976
EHO K. Hermann <hermannleboss@gmail.com>
2022-05-26 22:10:47 +0000

    routing biding ok

--- components/ButtonItem.vue
+++ components/ButtonItem.vue
@@ -1,22 +1,25 @@
 <template>
-  <b-button :variant="variant" class="d-flex align-items-center" squared>
+  <b-button class='d-flex align-items-center' squared
+            :variant='variant'
+            v-bind='$attrs'
+            v-on='$listeners'>
     <div
-      v-if="icon"
-      class="btn-icon d-flex align-items-center justify-content-center"
+      v-if='icon'
+      class='btn-icon d-flex align-items-center justify-content-center'
     >
       <svg
-        fill="none"
-        height="14"
-        viewBox="0 0 18 14"
-        width="18"
-        xmlns="http://www.w3.org/2000/svg"
+        fill='none'
+        height='14'
+        viewBox='0 0 18 14'
+        width='18'
+        xmlns='http://www.w3.org/2000/svg'
       >
-        <path d="M1 9L9 13L17 9" stroke="#FFF"/>
-        <path d="M1 5L9 9L17 5" opacity="0.5" stroke="#FFF"/>
-        <path d="M1 1L9 5L17 1" opacity="0.25" stroke="#FFF"/>
+        <path d='M1 9L9 13L17 9' stroke='#FFF' />
+        <path d='M1 5L9 9L17 5' opacity='0.5' stroke='#FFF' />
+        <path d='M1 1L9 5L17 1' opacity='0.25' stroke='#FFF' />
       </svg>
     </div>
-    <div class="btn-text">
+    <div class='btn-text'>
       <slot>About Me</slot>
     </div>
   </b-button>
@@ -29,14 +32,17 @@ export default {
   props: {
     variant: {
       type: [String],
-      default: 'primary',
+      default: 'primary'
     },
     icon: Boolean,
-  },
+    buttonClass: {
+      type: String
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang='scss' scoped>
 .btn {
   height: 48px;
   padding: 0;
--- components/PresentationCard.vue
+++ components/PresentationCard.vue
@@ -2,29 +2,29 @@
   <div
     :class="{ 'flex-row-reverse': realisation.id % 2=== 0 }"
 
-    class="custom-container-fluid presentation-spacing d-block d-md-flex m-0"
+    class='custom-container-fluid presentation-spacing d-block d-md-flex m-0'
   >
     <img fluid alt='Profile image'
-         class='img-full d-none d-xl-block mx-auto card-image' :src="realisation.images.image_portfolio"/>
+         class='img-full d-none d-xl-block mx-auto card-image' :src='realisation.images.image_portfolio' />
     <!--
     <img :src="require(`~/assets/images/portfolio/tablet/${imgUrlTablet}.jpg`)" alt='Profile image'
          class='img-full d-none d-md-block d-xl-none'/>
     <img :src="require(`~/assets/images/portfolio/mobile/${imgUrlMobile}.jpg`)" alt='Profile image'
          class='img-full d-block d-md-none mx-auto'/>-->
 
-    <div class="presentation-text align-items-stretch d-flex m-0 px-sm-5">
-      <div class="border-top border-bottom align-items-stretch mt-4 mt-md-0">
-        <h2 class="pt-4 py-sm-5 pb-4 m-0">{{ realisation.title }}</h2>
+    <div class='presentation-text align-items-stretch d-flex m-0 px-sm-5'>
+      <div class='border-top border-bottom align-items-stretch mt-4 mt-md-0'>
+        <h2 class='pt-4 py-sm-5 pb-4 m-0'>{{ realisation.title }}</h2>
         <p>{{ realisation.shortDesc }}
         </p>
-        <ButtonItem variant="outline-secondary">VIEW PROJECT</ButtonItem>
-        <div class="pt-5"></div>
+        <ButtonItem variant='outline-secondary' :to='"/portfolio/"+realisation.id'>VIEW PROJECT</ButtonItem>
+        <div class='pt-5'></div>
       </div>
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import Vue from 'vue'
 
 export default Vue.extend({
@@ -33,11 +33,13 @@ export default Vue.extend({
   props: {
     reverse: Boolean,
     realisation: Object
+  },
+  computed: {
   }
 })
 </script>
 
-<style lang="scss" scoped>
+<style lang='scss' scoped>
 .card {
   &-spacing {
     padding-top: 96px;
