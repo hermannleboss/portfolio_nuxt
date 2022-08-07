1ae60e5dbe5fd2e22ba3bcc4799ecf753a448025
Hermann EHO <hermannleboss@gmail.com>
2022-08-07 15:02:56 +0000

    add eslint recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,24 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
-    'prettier',
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:nuxt/recommended",
+    "prettier",
+    "eslint:recommended"
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser',
+    parser: "@babel/eslint-parser",
     ecmaVersion: 12,
     requireConfigFile: false,
-    sourceType: 'module',
+    sourceType: "module"
   },
-  plugins: ['@typescript-eslint', 'jest'],
+  plugins: ["@typescript-eslint", "jest"],
   // add your custom rules here
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-}
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
+};
